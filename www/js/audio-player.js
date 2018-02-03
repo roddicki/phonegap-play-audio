b1a7d29a16ccf2da080d3e7dcf728b4887780402
rod <rod@roddickinson.net>
2018-02-03 18:35:13 +0000

    complete rewrite

--- www/js/audio-player.js
+++ www/js/audio-player.js
@@ -1,37 +1,110 @@
 var audio = {
-	fileName: "",
-	isPlaying: false,
-	audioObject: "",
+	isPlaying: [],
+	objectList: {},
+	currPosition: 0,
 	getDevicePath: function() {
 	    var path = window.location.pathname;
 	    var sizefilename = path.length - (path.lastIndexOf("/")+1);
 	    path = path.substr( path, path.length - sizefilename );
 	    return path;
 	},
-	createAudioObject: function() {
-		var url = audio.getDevicePath() + audio.fileName;
+	createAudioObject: function(filepath) {
+		var url = audio.getDevicePath() + filepath;
 		console.log("\nfileppath used\n" + url);
 	    var mediaObject = new Media(url,
 	            // success callback
-	            function () { console.log("Audio object Success"); },
+	            function () { 
+	            	console.log("Audio has played - Success"); 
+	            	audio.deleteFromisPlaying(filepath); //deleteFromisPlaying
+	            },
 	            // error callback
 	            function (err) { console.log("Audio object Error: " + JSON.stringify(err) ); }
 	    );
-	    console.log(JSON.stringify(mediaObject));
 	    return mediaObject;
 	},
+	addToisPlaying: function(filepath) {
+		//add to isPlaying list
+		audio.isPlaying.push(filepath); 
+	},
+	deleteFromisPlaying: function(index) {
+		//delete from isPlaying list
+		audio.isPlaying.splice(index, 1); //
+	},
+	getObject: function(filepath) {
+		var audioObject;
+		if (!(("/"+filepath) in audio.objectList)) {
+			//object in objectList doesn't exist so create audioOject
+			audioObject = audio.createAudioObject(filepath);
+			audio.objectList[audioObject.src] = audioObject; //add to objectList
+			console.log(JSON.stringify(audio.objectList));
+		} else {
+			//object exists so find it and return it
+			audioObject = audio.objectList["/"+filepath];
+		};
+		return audioObject;
+	},
+	isCurrentlyPlaying: function(filepath) {
+		if (audio.isPlaying.indexOf(filepath) != -1) {
+			return true;
+		} else {
+			return false;
+		};	
+	},
 	play: function(filepath) {
-		/*if (audio.isPlaying) {
-			audioObject.pause();
-		};*/
-		audio.fileName = filepath;
-		var audioObject = audio.createAudioObject();
-		audioObject.play();
-		audio.isPlaying = true;
+		console.log(audio.isPlaying)
+		console.log(audio.isPlaying.indexOf(filepath));
+
+		var audioObject = audio.getObject(filepath);
+		//if audio is NOT playing
+		if (!(audio.isCurrentlyPlaying(filepath))) {
+			//is not playing 
+			audio.addToisPlaying(filepath); //add to list of playing audio
+			audioObject.play();	//play
+		};	
 	}, 
-	pause: function(){
-		console.log(JSON.stringify(audioObject) );
-		//audioObject.pause();
-		//audioObject.isPlaying = false;
+	pause: function(filepath) {
+		var isPlayingIndex = audio.isPlaying.indexOf(filepath);
+		console.log("audio.isCurrentlyPlaying: " + audio.isCurrentlyPlaying(filepath)); 
+		//if audio is playing
+		if (audio.isCurrentlyPlaying(filepath)) {
+			//is playing so find object and pause
+			var audioObject = audio.objectList["/"+filepath];
+			console.log(JSON.stringify(audioObject.src) + " is playing and will be paused" + "\n object: \n" + JSON.stringify(audioObject));
+			audioObject.pause();
+			//delete from isPlaying list
+			audio.deleteFromisPlaying(isPlayingIndex); 
+		};
+	},
+	getDuration: function(filepath) {
+		var audioObject = audio.getObject(filepath);
+		console.log("getDuration object:" + JSON.stringify(audioObject));
+		return audioObject.getDuration();
+	},
+	seekTo: function(filepath, milliseconds) {
+		var audioObject = audio.getObject(filepath);
+		audioObject.seekTo(milliseconds);
+	},
+	getCurrentPosition: function(filepath) {
+		var audioObject = audio.getObject(filepath);
+		// get media position
+	    audioObject.getCurrentPosition(
+	        // success callback
+	        function (position) {
+	            if (position > -1) {
+	                console.log((position) + " sec");
+	                audio.currPosition = position;
+	            }
+	        },
+	        // error callback
+	        function (e) {
+	            console.log("Error getting pos=" + e);
+	        }
+	    );
+	    return audio.currPosition;
 	}
 };
+
+
+
+
+
--- www/js/index.js
+++ www/js/index.js
@@ -3,30 +3,54 @@ document.addEventListener('deviceready', function() {
     console.log('\n-------------\nDEVICE READY');
     
    	//event listeners for 'play' and 'pause' buttons
+    //play audio file
+    //audio.play("audio/oboe.mp3");
     document.getElementById("play-btn-one").addEventListener("click", function(){
-        audio.play("audio/abcd.mp3");
+        audio.play("audio/oboe.mp3");
     });
 
+    //pause audio file
+    //audio.pause("audio/oboe.mp3");
+    document.getElementById("pause-one").addEventListener("click", function(){
+        audio.pause("audio/oboe.mp3");
+    });
+
+    //see the duration of an audio file 
+    //audio.getDuration("audio/oboe.mp3");
+    //only returns the duration once playing
+    document.getElementById("get-duration-one").addEventListener("click", function(){
+        document.getElementById("duration-one").innerHTML = audio.getDuration("audio/oboe.mp3");
+    });
+
+    //seek to / go to current position (milliseconds) within an audio file.
+    //audio.seekTo("audio/oboe.mp3", 50000);
+    //example: seek to 50 seconds and play
+    document.getElementById("seek").addEventListener("click", function(){
+        audio.seekTo("audio/oboe.mp3", 50000);
+        audio.play("audio/oboe.mp3");
+    });
+    
+    //get current position of audio file if playing
+    //audio.getCurrentPosition("audio/drums.mp3");
+    document.getElementById("position-one").addEventListener("click", function(){
+        document.getElementById("position-one-result").innerHTML = audio.getCurrentPosition("audio/oboe.mp3");
+    });
+
+    //event listeners for 'play' and 'pause' buttons
     document.getElementById("play-btn-two").addEventListener("click", function(){
-        audio.play("audio/white-christmas.mp3");
+        audio.play("audio/drums.mp3");
     });
 
-    document.getElementById("play-btn-three").addEventListener("click", function(){
-        audio.play("audio/alphaOmega.mp3");
+    document.getElementById("pause-two").addEventListener("click", function(){
+        audio.pause("audio/drums.mp3");
     });
 
-    document.getElementById("play-btn-four").addEventListener("click", function(){
-        audio.play("audio/xyz.mp3");
+    //get current position of audio file if playing
+    //audio.getCurrentPosition("audio/drums.mp3");
+    document.getElementById("position-two").addEventListener("click", function(){
+        document.getElementById("position-two-result").innerHTML = audio.getCurrentPosition("audio/drums.mp3");
     });
-
-    document.getElementById("play-btn-five").addEventListener("click", function(){
-        audio.play("audio/1234.mp3");
-    });
-
-    //event listener for 'pause' button
-    document.getElementById("pauseBtn").addEventListener("click", function(){
-        audio.pause();
-    });
+    
 
 //end device ready
 });
